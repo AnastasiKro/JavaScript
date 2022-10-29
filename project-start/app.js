423faff338d4f57b81854b880e3dd1fda02f43da
yaAvtobus <naskrop@gmail.com>
2022-10-30 00:53:57 +0300

    ended

--- project-start/app.js
+++ project-start/app.js
@@ -1,32 +1,33 @@
 
-function search_user(){
-	console.log('searching');
-}
-
-/*async function getUsers() {
-	console.log("hey");
-	try{
-		const response = await fetch('https://jsonplaceholder.typicode.com/users');
-		const data = await response.json();
-		data.forEach(el => console.log(el.name));
-	} catch (error){
-		console.error(error.message);
+function userbyName(name){
+	for (var i = 0; i<users.length; i++){
+		if (users[i].name === name){
+			return users[i].id;
+		}
 	}
-}*/
-
+}
 function handleClick(event){
 	const newTodo = document.getElementById('new-todo').value;
 	event.preventDefault();
-	//console.log(newTodo);
-	if (newTodo){
-		postTodo({
-			userID: 0,
+	const us = document.getElementById('user-todo').value;
+	if (newTodo && (us != "select user")){
+		const obj = {
+			userId: userbyName(us),
 			title: newTodo,
 			completed: false,
+		};
+		const post = postTodo(obj)
+		.then((data) => {
+			if (data.id === 201){
+				createTodo(obj)
+			}
+		})
+		.catch((error) => {
+			console.log(error);
 		});
 		document.getElementById('new-todo').value = '';
 	} else {
-		alert('empty');
+		alert('empty field');
 	}
 }
 function userbyID(userID){
@@ -38,19 +39,39 @@ function userbyID(userID){
 }
 
 function createTodo(value) {
-	const div = document.createElement('div');
-	div.className = 'todo-item';
-	div.innerHTML += ('<input type="checkbox" id="check">' + `<span>${value.title} <i> by </i> <b>${userbyID(value.userId)}</b></span>`);//+ value.title + "by"
-	div.innerHTML += ('<div class="close">x</div>')
-	list.append(div)
-	if (value.completed){
-		div.querySelector("#check").checked = true;
-	}
+	const li = document.createElement('li');
+	li.id = value.id;
+	li.className = 'todo-item';
+	li.innerHTML = `<span>${value.title} <i> by </i> <b>${userbyID(value.userId)}</b></span>`;
+	const ch = document.createElement('input');
+	ch.type = 'checkbox';
+	ch.checked = value.completed;
+	ch.addEventListener('change', checkboxChange)
+	li.prepend(ch);
+	const x = document.createElement('span');
+	x.innerHTML = '&times;';
+	x.className = 'close';
+	x.addEventListener('click', handleClose);
+	li.append(x);
+	list.prepend(li);
+}
+function checkboxChange(){
+	const id = this.parentElement.id;
+	const completed = this.checked;
+	patchTodo(id, completed);
 }
 
-function removeToDo(){
-	this.removeEventListener();
-	this.remove();
+function removeToDo(id){
+	alltodos = alltodos.filter((todo) => todo.id !== id);
+	remTodo = list.querySelector(`[id="${id}"]`);
+	remTodo.querySelector('input').removeEventListener('change', checkboxChange);
+	remTodo.querySelector('.close').removeEventListener('click', handleClose);
+	remTodo.remove();
+}
+
+function handleClose(){
+	const id = this.parentElement.id;
+	deleteTodo(id);
 }
 
 function createUser(user){
@@ -66,10 +87,7 @@ const list = document.getElementById('todo-list');
 const btn = document.querySelector('button');
 let alltodos = [];
 let users = [];
-//const select_users = document.querySelector('option')
 btn.addEventListener('click', handleClick);
-todo.addEventListener('input', _.debounce(search_user, 1000));
-//user.addEventListener('click', getUsers);
 initToDos();
 
 async function GetToDos(){
@@ -94,12 +112,52 @@ async function postTodo(did){
 			},
 		});
 		const data = await response.json();
-		console.log(data);
+		if (response.ok){
+			return data;
+		}
 	} catch (error){
 		console.error(error.message)
 	}
 }
-		
+	
+async function patchTodo(id, completed){
+	try{
+		const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`,{
+			method: 'PATCH',
+			body: JSON.stringify({completed}),
+			headers:{
+				'Content-Type': 'application/json',
+			},
+		});
+		const data = await response.json();
+		if (response.ok){
+			return data;
+		} else{
+			throw new Error('Failed to patch');
+		}
+	} catch (error){
+		console.error(error.message)
+	}
+}	
+
+async function deleteTodo(id){
+	try{
+		const response = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}`,{
+			method: 'DELETE',
+			headers:{
+				'Content-Type': 'application/json',
+			},
+		});
+		if (response.ok){
+			removeToDo(id);
+		}
+		else{
+			throw new Error('Failed to delete');
+		}
+	} catch (error){
+		console.error(error.message)
+	}
+}	
 
 async function GetUsers(){
 	try {
@@ -113,7 +171,6 @@ async function GetUsers(){
 	}
 }
 
-
 function initToDos(){
 	Promise.all([GetToDos(), GetUsers()]).then((values) => {
 		[alltodos, users] = values;
@@ -121,9 +178,5 @@ function initToDos(){
 		users.forEach((user) => createUser(user));
 	});
 }
-//TODOS.forEach((t) => console.log(t.title));
-//fetch('https://jsonplaceholder.typicode.com/posts/1', {
-//	method: 'POST',
-//	body: JSON.stringify({
-//		title: ''
+
 
