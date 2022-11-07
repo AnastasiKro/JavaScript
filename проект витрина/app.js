9f878243c4a4d3b107840775d0ea69e719c5c93f
Anastasiia <62606871+yaAvtobus@users.noreply.github.com>
2022-11-07 14:51:04 +0300

    making it pretty

--- проект витрина/app.js
+++ проект витрина/app.js
@@ -19,38 +19,28 @@ const products = [
 let order = [];
 
 function addToBasket(productId) {
-    // TODO: добавить проверку наличия товара в заказе (при наличии выдать alert, что товар уже в корзине)
 	if(order[productId-1] != null)
 		alert('This product is already in the basket')
-    // TODO: если товар еще не в корзине, добавить его из массива products
 	else{
 	order[productId-1] = products[productId-1]
 	}
-    // Эти строчки не трогаем, они отвечают за переотрисовку страницы
     renderCart();
     rerenderTotalPrice();
 }
 
 function removeFromBasket(productId) {
-    // TODO: описать логику удаления товара из корзины
 	delete order[productId-1]
-	
-    // Эти строчки не трогаем, они отвечают за переотрисовку страницы
-    renderCart();
-    rerenderTotalPrice();
+	renderCart();
+	rerenderTotalPrice();
 }
 
 
 function rerenderTotalPrice() {
-    // TODO: опишите функционал подсчета общей стоимости заказа
 	let totalPrice = 0
 	order.forEach(el => totalPrice+=el.price)
-
-    // Не меняйте эту строчку
-    document.getElementById('total').innerText = totalPrice;
+	document.getElementById('total').innerText = totalPrice;
 }
 
-// Этот метод остается без изменений
 function renderCart() {
     const cart = document.getElementById('basket-items');
 
